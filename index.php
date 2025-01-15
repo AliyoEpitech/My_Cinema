a17605b72a12eabdbc9e9090a19689e0242fd309
AliyoEpitech <aliyo.moussa@epitech.eu>
2025-01-15 11:36:09 +0100

    récupération des valeur du tableau

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php 
- 
 $server = "localhost";
 $user = "root";
 $password = "aliyo1004";
 $table = "cinema";
- 
+
+// try et catch permette de gerer l'erreur.
 try {
     $connexion = new PDO("mysql:host=$server;dbname=$table", "$user", "$password");
-    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $requete = $connexion->prepare("SELECT * FROM genre;");
     $requete->execute();
     $resultat = $requete->fetchall();
-    foreach()
-    echo"<pre>";
-      print_r($resultat);
-    echo "<pre>";
-} catch (PDOException $e) {
+    foreach($resultat as $valuetbl) {
+        $name = $valuetbl["name"];
+        $id = $valuetbl["id"];
+        echo "<pre>";
+        echo "$id.$name";
+        echo "<pre>";
+    }
+} catch (PDOException) {
     die("Echec de la connexion");
 }
-
-// echo "Connexion à la base de données réussie";
-// $requete = $connexion->query("SELECT* FROM job");
-// echo $requete;
