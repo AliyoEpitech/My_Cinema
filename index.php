726f1146136af4c6f32f2d4566b4aaa1a0b4e974
AliyoEpitech <aliyo.moussa@epitech.eu>
2025-01-16 11:01:50 +0100

    Fussion de table avec innerJOIN

--- index.php
+++ index.php
@@ -8,12 +8,13 @@ $table = "cinema";
 try {
     $connexion = new PDO("mysql:host=$server;dbname=$table", "$user", "$password");
     $connexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $requete = $connexion->prepare("SELECT * FROM genre;");
+    $requete = $connexion->prepare("SELECT * FROM movie INNER JOIN distributor ON movie.id_distributor = distributor.id;");
     $requete->execute();
     $resultat = $requete->fetchall();
     foreach($resultat as $valuetbl) {
         $name = $valuetbl["name"];
         $id = $valuetbl["id"];
+        $distributor = $valuetbl["title"];
         echo "<pre>";
         echo "$id.$name";
         echo "<pre>";
